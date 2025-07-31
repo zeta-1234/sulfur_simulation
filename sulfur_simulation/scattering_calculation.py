c738e39645576afc02f61d3c0b2ca10728d2fba0
zeta-1234 <benjamincollishaw@gmail.com>
2025-07-31 15:20:53 +0000

    Added functions to calculate delta_k and update a particle position

--- sulfur_simulation/scattering_calculation.py
+++ sulfur_simulation/scattering_calculation.py
@@ -5,6 +5,34 @@ from typing import Any
 
 import numpy as np
 
+n_directions = 4
+directions = {
+    0: np.array([1, 0]),  # RIGHT
+    1: np.array([-1, 0]),  # LEFT
+    2: np.array([0, 1]),  # UP
+    3: np.array([0, -1]),  # DOWN
+}
+rng = np.random.default_rng()
+
+
+def get_delta_k(
+    angle_of_incidence: float, total_scattering_angle: float, wavevector: np.ndarray
+) -> np.ndarray[Any, np.dtype[np.float64]]:
+    """Calculate delta_k for given angles and incident wavevector."""
+    return wavevector * (
+        np.sin(total_scattering_angle - angle_of_incidence) - np.sin(angle_of_incidence)
+    )
+
+
+def update_position(
+    position: np.ndarray, hopping_probability: float
+) -> np.ndarray:  # temp function to enable simple random diffusion
+    """Update the position of the particle."""
+    if rng.random() < hopping_probability:
+        direction = int(rng.integers(low=0, high=n_directions))
+        return position + directions[direction]
+    return position
+
 
 def get_amplitude(
     delta_k: np.ndarray,
