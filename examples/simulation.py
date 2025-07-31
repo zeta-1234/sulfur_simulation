f99375d4ac0f909fed19e21a8ba6ac1ec3fa6cc8
Matt Ord <matthew.ord1@gmail.com>
2025-07-31 13:49:25 +0000

    More review

--- examples/simulation.py
+++ examples/simulation.py
@@ -4,4 +4,4 @@ from sulfur_simulation.scattering_calculation import SimulationParameters
 
 if __name__ == "__main__":
     # Create a simulation with 5000 timesteps
-    params = SimulationParameters(delta=1.2, duration=5000, gamma=1, step=2)
+    params = SimulationParameters(delta=1.2, n_timesteps=5000, gamma=1, step=2)
