ec853751c796364a961a178ba5fd5383fd568296
zeta-1234 <benjamincollishaw@gmail.com>
2025-07-31 14:24:45 +0000

    Fixed params in simulation.py

--- examples/simulation.py
+++ examples/simulation.py
@@ -1,7 +1,15 @@
 from __future__ import annotations
 
+import numpy as np
+
 from sulfur_simulation.scattering_calculation import SimulationParameters
 
 if __name__ == "__main__":
     # Create a simulation with 5000 timesteps
-    params = SimulationParameters(delta=1.2, n_timesteps=5000, gamma=1, step=2)
+    params = SimulationParameters(
+        total_scattering_angle=1.2,
+        angle_of_incidence=0.8,
+        n_timesteps=5000,
+        step=2,
+        incident_wavevector=np.array([3, 6]),
+    )
